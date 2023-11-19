0f8182142e20bdfb81b7c9d5c841721002a4be15
anthonyMeds <anthony.eduardo@hotmail.com>
2023-11-19 20:34:46 -0300

    #1 -implementação crud produtos - wip

---
+++ src/main/java/br/com/dermocosmeticos/appDermocosmeticos/Model/Entity/Produto.java
@@ -0,0 +1,30 @@
+package br.com.dermocosmeticos.appDermocosmeticos.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "tb_produto")
+public class Produto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "produto_id")
+    private Long produtoId;
+
+    @Column(name = "produto_nome")
+    private String produtoNome;
+
+    @Column(name = "produto_categoria")
+    private String produtoCategoria;
+
+    @Column(name = "produto_marca")
+    private String produtoMarca;
+
+    @Column(name = "preco_unitario")
+    private BigDecimal precoUnitario;
+}
